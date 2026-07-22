56db1af326d3a7e943b37fa504b59d1deac94190
Tom Pekrul <tom.pekrul1@gmail.com>
2026-07-23 01:25:59 +0200

    add intel-media-driver (hardware acceleration)

--- configuration.nix
+++ configuration.nix
@@ -20,6 +20,10 @@
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
+
+    extraPackages = with pkgs; [
+      intel-media-driver
+    ];
   };
 
   services.xserver.videoDrivers = [
